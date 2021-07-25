0db4c3709d23b0c087a3168ce933746849006f75
Sushma Vadla <sushmavadla@Sushmas-MacBook-Pro.local>
2021-07-24 17:04:02 -0700

    Adding some real code

---
+++ ProjectCode.R
@@ -0,0 +1,31 @@
+###########################
+# Polygence Project #
+# Date: 6/26/21         #
+# Subha        #
+###########################
+
+install.packages('ggplot2')
+install.packages('readxl')
+install.packages("lattice")
+install.packages('dplyr')
+install.packages('ggiraphExtra')
+install.packages('stargazer')
+install.packages('xtable')
+install.packages("kableExtra")
+install.packages('tidyverse')
+install.packages('lmtest')
+install.packages('mosaic')
+library(mosaic)
+library(lmtest)
+library("kableExtra")
+library('xtable')
+library('stargazer')
+library('dplyr')
+library('readxl')
+library('ggplot2')
+library('lattice')
+library(ggiraphExtra)
+library(tidyverse)
+
+install.packages("devtools")
+devtools::install_github("cardiomoon/ggiraphExtra")
